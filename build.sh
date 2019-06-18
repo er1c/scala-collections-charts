d9a4df5eaeb4d67f80889ee83cabd19d6752edac
Mike Slinn <mslinn@micronauticsresearch.com>
2019-06-17 18:06:59 -0700

    First cut at a Scala 2.13 version

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 rm -Rf target
 mkdir -p target
 
+if [[ -z "$( which dot )" ]]; then
+  echo "the Graphviz dot program is not installed. For Ubuntu and WSL type:"
+  echo "sudo apt install python-pydot python-pydot-ng graphviz"
+  exit 1
+fi
+
 echo "Generating SVGs and PSs in /target directory..."
 
 for f in src/*.gv
